f48e607db625b031a9ba3aa190db1e300a404801
HarrisonHemstreet <harrisonhemstreet@gmail.com>
2021-06-23 22:30:17 -0600

    added Products Page Grid View and List View

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,7 +4,19 @@ import GridView from './GridView'
 import ListView from './ListView'
 
 const ProductList = () => {
-  return <h4>product list</h4>
+  // change "all_products" to be filtered_products when done? 315
+  const {all_products: products, grid_view} = useFilterContext();
+  if(products.length < 1) {
+    return (
+    <h5 style={{textTransform: "none"}}>
+      Sorry, no products matched your search...
+    </h5>
+    )
+  }
+  if(grid_view === false) {
+    return <ListView products={products} />
+  }
+  return <GridView products={products}>Product List</GridView>
 }
 
 export default ProductList
