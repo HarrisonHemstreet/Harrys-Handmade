e2efb20016ad523e95a595f87314d50625f7c9ad
HarrisonHemstreet <harrisonhemstreet@gmail.com>
2021-06-23 19:24:34 -0600

    added Single Product loading and error catching

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -14,7 +14,35 @@ import {
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
+
 const SingleProductPage = () => {
+  const history = useHistory();
+  const { id } = useParams(); 
+  const {
+    single_product_loading: loading,
+    single_product_error: error, 
+    single_product: product, 
+    fetchSingleProduct,
+  } = useProductsContext();
+
+  useEffect(() => {
+    fetchSingleProduct(`${url}${id}`);
+  }, [id]);
+
+  useEffect(() => {
+    if(error) {
+      setTimeout(() => {
+        history.push('/');
+      }, 3000);
+    }
+  }, [error])
+  
+  if(loading) {
+    return <Loading />
+  }
+  if(error) {
+    return <Error />
+  }
   return <h4>single product page</h4>
 }
 
