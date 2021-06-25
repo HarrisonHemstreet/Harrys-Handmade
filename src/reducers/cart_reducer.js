96e7f989a4b142e44b8b11cd6476b9c394414cc2
HarrisonHemstreet <harrisonhemstreet@gmail.com>
2021-06-24 21:48:00 -0600

    cart setup page

--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -14,7 +14,7 @@ const cart_reducer = (state, action) => {
       const tempCart = state.cart.map((cartItem) => {
         if(cartItem.id === id + color) {
           let newAmount = cartItem.amount + amount;
-          if(newAmount > carItem.max) {
+          if(newAmount > cartItem.max) {
             newAmount = cartItem.max
           }
           return { ...cartItem, amount: newAmount }
